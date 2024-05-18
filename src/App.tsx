345de77309a199bb4fd59bb79066bf0b80213ebe
hb351 <boustta.elh@gmail.com>
2024-05-18 13:50:25 +0100

    build the initial Navigation Bar form

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
@@ -8,8 +9,8 @@ function App() {
         lg: `"nav nav" "Aside Main"`,
       }}
     >
-      <GridItem area={"nav"} bg={"orange"}>
-        Nav
+      <GridItem area={"nav"}>
+        <NavBar></NavBar>
       </GridItem>
       <Show above="lg">
         <GridItem area={"Aside"} bg={"blue"}>
